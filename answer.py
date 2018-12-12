84f106a436931c0020062569cba520642f6c9c82
zerg2008 <42507592@qq.com>
2018-12-12 00:52:15 -0800

    矩阵转换

---
+++ answer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arryList = [7,9,6,2,5,3]
+
+print(arryList)
+
+reshapArrayList = np.array(arryList)
+
+reshapArrayList = reshapArrayList.reshape((3,2))
+
+print(reshapArrayList)
